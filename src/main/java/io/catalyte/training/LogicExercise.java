3909a515bac05aaa12e1c44a9d6233b9b0ad12f6
Phil <pfestoso@redhat.com>
2018-08-07 10:40:25 -0500

    first methods

---
+++ src/main/java/io/catalyte/training/LogicExercise.java
@@ -0,0 +1,56 @@
+package io.catalyte.training;
+
+/**
+ * @author pfestoso
+ *
+ */
+public class LogicExercise {
+	
+	
+	/**
+	 * This method should take an double array, a, and return a new array that contains the square of each element in a.
+	 *
+	 */
+	public double[] powerArray(double[] a) {
+		return null;
+	}
+	
+	
+	/**
+	 * This method should take an int array, a, and return the index of the element with the greatest value.
+	 *
+	 */
+	public int indexOfMax(int[] a) {
+		return -1;
+	}
+	
+	
+	/**
+	 * This method should take an int array, a, and returns true if all elements in the array
+	 * are divisible by the given integer, i.
+	 *
+	 */
+	public boolean indexOfMax(int i, int[] a) {
+		return false;
+	}
+	
+	
+	/**
+	 * A word is "abecedarian" if its letters appear in alphabetical order--the word 'biopsy' for example.
+	 * This method should take String s and return true if it is abecedarian.
+	 *
+	 */
+	public boolean isAbecedarian(String s) {
+		return false;
+	}
+	
+	/**
+	 * This method should take 2 arrays and return true if they are anagrams of each other. 
+	 * For example, "stop" is an anagram for "pots".
+	 *
+	 */
+	public boolean areAnagrams(String s1, String s2) {
+		return false;
+	}
+	
+}
